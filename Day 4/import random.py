36c76ff495b79f656e81d6becc9445f4543edbab
hai <hai.sarusi@gmail.com>
2025-02-02 10:29:46 +0200

    added piza calculator

--- Day 4/import random.py
+++ Day 4/import random.py
@@ -7,4 +7,4 @@ random_number_0_1 = random.random()
 print(f"this is a random number between 0 and 1: {random_number_0_1}")
 
 random_float = random.uniform(1, 10)
-print(f"this is a random float between 1 and 10: {random_float}")
+print(f"this is a random float between 1 and 10: {random_float}")git pull --ff-only
